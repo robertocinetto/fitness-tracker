372ef69821a6b4c2319f5e90f6e0911bcb029ef7
Roberto Cinetto <roberto.cinetto@gmail.com>
2022-09-27 18:57:07 -0700

    entry deletion functionality

--- components/FitnessDataTable.js
+++ components/FitnessDataTable.js
@@ -1,7 +1,9 @@
 import { DataTable } from 'primereact/datatable'
 import { Column } from 'primereact/column'
+import { Button } from 'primereact/button'
+
 import { useEffect, useState } from 'react'
-import { collection, onSnapshot, query, where } from 'firebase/firestore'
+import { collection, onSnapshot, query, where, deleteDoc, doc } from 'firebase/firestore'
 import { db } from '../firebase'
 import { useRecoilState } from 'recoil'
 import { userState } from '../atom/userAtom'
@@ -13,8 +15,23 @@ const FitnessDataTable = () => {
   useEffect(() => {
     if (currentUser) {
       const unsub = onSnapshot(query(collection(db, 'records'), where('username', '==', currentUser.username)), collection => {
-        setRecords(collection.docs.map(record => record.data()))
-        // console.log(collection.docs.map(record => record.data()))
+        //since the snapshot doesn't get the id of each doc, ids needs to be collected in another way
+        //I had to get ids to be able to delete single docs from the collection
+
+        //get the collection docs
+        const recordsCollection = collection.docs
+
+        //build an array of ids from the collection
+        let ids = recordsCollection.map(record => record.id)
+
+        //build an array of objects containing the db data
+        let recordsData = recordsCollection.map(record => record.data())
+
+        //build an alternative array chaining ids and data
+        const _records = recordsData.map((record, i) => {
+          return { ...record, id: ids[i] }
+        })
+        setRecords(_records)
       })
     }
   }, [currentUser])
@@ -37,7 +54,36 @@ const FitnessDataTable = () => {
     )
   })
 
-  return <DataTable value={records}>{dynamicColumns}</DataTable>
+  const actionBodyTemplate = rowData => {
+    return (
+      <>
+        {/* <Button
+          icon="pi pi-pencil"
+          className="p-button-rounded p-button-success mr-2"
+          onClick={() => editProduct(rowData)}
+        /> */}
+        <Button
+          icon="pi pi-trash"
+          className="p-button-rounded p-button-warning"
+          onClick={() => confirmDeleteProduct(rowData)}
+        />
+      </>
+    )
+  }
+
+  const confirmDeleteProduct = async rowData => {
+    await deleteDoc(doc(db, 'records', rowData.id))
+  }
+
+  return (
+    <DataTable value={records}>
+      {dynamicColumns}
+      <Column
+        body={actionBodyTemplate}
+        exportable={false}
+      ></Column>
+    </DataTable>
+  )
 }
 
 export default FitnessDataTable
